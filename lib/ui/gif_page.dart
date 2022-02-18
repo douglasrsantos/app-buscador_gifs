0dc99812ba9faee834ad5ef307c9751588b25805
Douglas da Rosa Santos <douglasrosasantos@gmail.com>
2022-02-18 16:07:37 -0300

    commit final

---
+++ lib/ui/gif_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:share/share.dart';
+
+class GifPage extends StatelessWidget {
+  //const GifPage({Key key}) : super(key: key);
+
+  final Map _gifData;
+
+  const GifPage(this._gifData);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_gifData['title']),
+        backgroundColor: Colors.black,
+        actions: [
+          IconButton(onPressed: () {
+              Share.share(_gifData['images']['fixed_height']['url']);
+            }, icon: const Icon(Icons.share),
+          ),
+        ],
+      ),
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Image.network(_gifData['images']['fixed_height']['url']),
+      ),
+    );
+  }
+}
